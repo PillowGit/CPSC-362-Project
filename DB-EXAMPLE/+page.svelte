e88df331c59cab1315c14596780919a48f0f9fbe
PillowGit <goldentaco727@gmail.com>
2024-03-05 12:43:38 -0800

    Moved database setup/practice to temp folder

---
+++ DB-EXAMPLE/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+  export let data;
+</script>
+
+<h1>
+  Home/Landing page
+</h1>
+
+<a href="login/">
+  <button>
+    Login
+  </button>
+</a>
+<p></p>
+
+{#if (data.isAuthed)}
+  <a href="home/">
+    <button class="reuse-login">
+      Sign into previous session
+    </button>
+  </a>
+{/if}
+
+<style>
+  button {
+    height: 90px;
+    width: 180px;
+    font-size: 2rem;
+  }
+  .reuse-login {
+    height: 120px;
+    width: 180px;
+    font-size: 1.75rem;
+  }
+</style>
