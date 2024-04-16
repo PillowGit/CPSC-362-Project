08859ca69ae4e47ca49881b72c8eedc6ad9cb442
PillowGit <goldentaco727@gmail.com>
2024-04-16 15:00:06 -0700

    Made references to list pages in account page

--- src/routes/list/[list_id]/+page.svelte
+++ src/routes/list/[list_id]/+page.svelte
@@ -1,28 +1,22 @@
 <script>
-    // This is the page where the actual to-do list is displayed
-    // We have /list/[username] so that if someone did ourwebsite/list/pillow
-    // we can check if they have the auth for pillow in their cookie list
-    // and display the to-do list if they do, or ask them to login if they don't
-
-    // Whether or not they have access to this page will be stored in data.authorized
-    export let data;
-
-    // A template for how this can be implemented in svelte is below:
-    // Please implement html for both authed and not authed
     import Navbar from '$lib/navbar.svelte';
     import PageColor from '$lib/pagecolor.svelte';
+
+    export let data;
 </script>
 
 <PageColor />
 <Navbar />
 
-{#if data.authorized}
-    <h1>Here's your to-do list:</h1>
-    <p>Here's where we'll display the to-do list</p>
-{:else}
-    <h1>Log in to access this page</h1>
-    <p>Here's where we'll display the login form</p>
-{/if}
+<h1>
+    <br>
+    <br>
+    <br>
+    Todo:
+    <br>
+    <br>
+    Make a todo list
+</h1>
 
 <style>
 </style>
